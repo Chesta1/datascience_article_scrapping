d8bc28fee7fab22b55782c5736e4fa49c30f90ca
Chesta1 <chestadhingra25@gmail.com>
2024-01-22 22:01:25 +0530

    setup2

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setup import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name ='python_package',
-    version = 0.1,
-    packages = find_packages()
-    install=[pandas,
-             selenium,
-             datetime],
+    name='python_package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'selenium',
+        'datetime'
+    ],
     author="Chesta Dhingra",
-    author_email = "chestadhingra25@gmail.com",
+    author_email="chestadhingra25@gmail.com",
     license='MIT',
-    description = "A python package for webscraping the latest articles on popular Data science blogs",
-    long_description=open('README.md').read()
-    url = ""
-
+    description="A python package for web scraping the latest articles on popular Data science blogs",
+    long_description=open('README.md').read(),
+    url="https://github.com/Chesta1/datascience_article_scrapping"  # Replace with your URL or remove if not applicable
 )
