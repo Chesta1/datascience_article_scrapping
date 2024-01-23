67cc1aa07de0bb454a9f8490a29c9c284dd8ab6d
Chesta1 <chestadhingra25@gmail.com>
2024-01-23 19:17:31 +0530

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_package',
+    name='datascraping_article',
     version='0.1',
     packages=find_packages(),
     install_requires=[
